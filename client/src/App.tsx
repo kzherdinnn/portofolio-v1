8c4df19bc4c880d9fbecebd887f2cf998664e28b
kzherdinnn <herdinkz2003@gmail.com>
2026-02-11 00:08:53 +0700

    chore: revert to version f947a96 for 3-column layout stability

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,6 @@
 import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import ProjectDetail from "./pages/projectDetail";
-import CertificateDetail from "./pages/CertificateDetail";
 import Animate from "./utils/animations/Animate";
 import { FaGithub, FaInstagram } from "react-icons/fa";
 import { CiLinkedin } from "react-icons/ci";
@@ -129,7 +128,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/project/:slug" element={<ProjectDetail />} />
-          <Route path="/certificate/:id" element={<CertificateDetail />} />
           <Route path="/login" element={<Login />} />
 
           {/* Protected Admin Routes */}
